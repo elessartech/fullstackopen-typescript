d6c3b4fb6a6d985be656df119d6ec7a5c2695191
Maksim Ilmast <maksim.ilmast@yandex.com>
2022-10-07 23:57:37 +0300

    9.15

--- react-with-types/src/components/Content.tsx
+++ react-with-types/src/components/Content.tsx
@@ -6,13 +6,26 @@ interface MyProps {
 
 interface CoursePart {
     name: string;
-    exerciseCount: number
+    exerciseCount: number;
+    description?: string;
+    type: string;
+    groupProjectCount?: number;
+    exerciseSubmissionLink?: string;
+    requirements?: string[];
 }
 
 const Content = (props: MyProps): JSX.Element => {
     return (
         <div>
-            {props.courseParts.map(coursePart => <p>{coursePart.name} {coursePart.exerciseCount}</p>)}
+            {props.courseParts.map(coursePart => 
+                <p>
+                    <strong>{coursePart.name} {coursePart.exerciseCount}</strong> 
+                    {coursePart.description ? <p><i>{coursePart.description}</i></p> : null}
+                    {coursePart.groupProjectCount ? <p>project exercises {coursePart.groupProjectCount}</p> : null}
+                    {coursePart.exerciseSubmissionLink ? <p>submit to <a href={coursePart.exerciseSubmissionLink}>{coursePart.exerciseSubmissionLink}</a></p> : null}
+                    {coursePart.requirements ? <p>required skills: {coursePart.requirements.join(", ")}</p> : null}
+                </p>
+            )}
         </div>
     )
 }
