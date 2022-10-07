917e92d306a30394203868b06ea9fd64cb855e32
Maksim Ilmast <maksim.ilmast@yandex.com>
2022-10-07 16:52:31 +0300

    9.12

--- patientor-backend/services/patientService.ts
+++ patientor-backend/services/patientService.ts
@@ -1,6 +1,7 @@
 import patientData from '../data/patients.json';
+import { v1 as uuid } from 'uuid'
 
-import { PatientEntry, NonSensitivePatientEntry } from '../types'
+import { PatientEntry, NonSensitivePatientEntry, NewPatientEntry } from '../types'
 
 const getEntries = (): PatientEntry[] => {
     return patientData;
@@ -10,12 +11,17 @@ const getNonSensitivePatientEntry = (): NonSensitivePatientEntry[] => {
     return patientData.map(({id, name, dateOfBirth, gender, occupation}) => ({id, name, dateOfBirth, gender, occupation}));
 };
 
-const addDiagnose = () => {
-    return null;
+const addPatient = ( entry: NewPatientEntry ): PatientEntry => {
+    const newPatientEntry = {
+        id: uuid(),
+        ...entry
+      };
+      patientData.push(newPatientEntry);
+      return newPatientEntry;
 };
 
 export default {
   getEntries,
   getNonSensitivePatientEntry,
-  addDiagnose
+  addPatient
 };
