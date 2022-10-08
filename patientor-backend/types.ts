e14e528c35dc5c47ee6e5c50b3ed7a872ec2262a
Maksim Ilmast <maksim.ilmast@yandex.com>
2022-10-09 02:08:47 +0300

    9.19

--- patientor-backend/types.ts
+++ patientor-backend/types.ts
@@ -10,9 +10,51 @@ export enum Gender {
     other = "other"
 } 
 
-export interface Entry {
-    
+export interface Discharge {
+    date: string;
+    criteria: string;
 }
+
+export interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: string[];
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+  }
+  
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface PatientEntry {
     id: string;
