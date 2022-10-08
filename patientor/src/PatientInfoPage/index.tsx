1014cc78976269187a00a37c21d8af2952a4f121
Maksim Ilmast <maksim.ilmast@yandex.com>
2022-10-08 22:41:01 +0300

    9.17

---
+++ patientor/src/PatientInfoPage/index.tsx
@@ -0,0 +1,47 @@
+import { Box, Typography } from "@material-ui/core";
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+import { Patient } from "../types";
+
+const PatientInfoPage = () => {
+    const { id } = useParams<{ id: string }>();
+    const [{ patient }, dispatch] = useStateValue();
+    useEffect(() => {
+        if (id) {
+            const fetchPatientInfo = async () => {
+                try {
+                  const { data: patientInfoFromApi } = await axios.get<Patient>(
+                    `${apiBaseUrl}/patients/${id}`
+                  );
+                  dispatch({ type: "SET_PATIENT_INFO", payload: patientInfoFromApi });
+                } catch (e) {
+                  console.error(e);
+                }
+              };
+              void fetchPatientInfo();
+        }
+    }, [id])
+    if (!patient || Object.keys(patient).length === 0) return null
+    console.log(patient)
+    return (
+        <div className="PatientInfo">
+            <Box marginTop={'2em'}>
+                <Typography align="left" variant="h5" style={{ fontWeight: 700 }}>
+                    {`${patient["data"].name} ${patient["data"].gender === "male" ? `♂` :  patient["data"].gender === "female" ? `♀` : `⚥`}`}
+                </Typography>
+                <Typography align="left" style={{ marginTop: "1em" }}>
+                    {`ssn: ${patient["data"].ssn}`}
+                </Typography>
+                <Typography align="left">
+                    {`occupation: ${patient["data"].occupation}`}
+                </Typography>
+            </Box>
+        </div>
+    );
+};
+
+export default PatientInfoPage;
+
